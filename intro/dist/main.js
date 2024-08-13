2056a1e2669e62c2b074999ff1c7cc2ba2fe56c6
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-08-13 12:58:08 -0300

    Configurações básicas

--- intro/dist/main.js
+++ intro/dist/main.js
@@ -1,1 +1,1 @@
-console.log("Hello Webpack 2");
+console.log("Mudamos a configuração!");
---
+++ intro/webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+module.exports = {
+    // Arquivo de entrada
+    entry: './src/teste.js',
+    // Arquivo de saída
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    }
+}
