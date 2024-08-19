6b3a80464760dd84553a4a4289d0e169d45d4ac3
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-08-19 14:35:45 -0300

    Leitura do arquivo .env

--- plugins/src/index.js
+++ plugins/src/index.js
@@ -2,7 +2,8 @@ import Heading from "./components/heading";
 
 const heading = new Heading();
 
-heading.create("Página principal")
+heading.create("Página principal");
 
-console.log(VERSION)
-console.log(PORT)
+console.log(VERSION);
+console.log(PORT);
+console.log(process.env.API_KEY);
