318d07de5a6783f9b33adbb3cec02dc7991deb4b
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-08-19 15:54:03 -0300

    Separação do ambiente de desenvolvimento e produção

---
+++ mode/webpack.dev.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    plugins: [
+        new TerserPlugin()
+    ]
+}
