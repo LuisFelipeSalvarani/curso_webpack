680288ed0519eb1d503feb3a5eb5aa267e86273a
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-08-22 13:13:08 -0300

    Module Federation: App da Contact

---
+++ module_federation/packages/contact/src/bootstrap.js
@@ -0,0 +1,7 @@
+import Contact from "./Contact";
+import React from "react";
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Contact />);
